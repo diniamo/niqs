93931efb543371e524a5e0b25e1947073f3fa7b1
diniamo <diniamo69@gmail.com>
2024-02-25 00:02:55 +0100

    fix volume binds

--- home-manager/hyprland/binds.nix
+++ home-manager/hyprland/binds.nix
@@ -107,8 +107,8 @@ in {
       "${mod}${ctrl}, k, resizeactive, 0 -50"
       "${mod}${ctrl}, l, resizeactive, 50 0"
   
-      ", XF86AudioRaiseVolume, exec, wpctl, set-volume @DEFAULT_AUDIO_SINK@ 5%+"
-      ", XF86AudioLowerVolume, exec, wpctl, set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+      ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+      ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
   
       ", XF86MonBrightnessUp, exec, brightnessctl set 2%+"
       ", XF86MonBrightnessDown, exec, brightnessctl set 2%- -n"
