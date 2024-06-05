b14836d0198894580e8695c90f326231874bdfde
diniamo <diniamo53@gmail.com>
2024-06-05 21:40:53 +0200

    home/dunst: change frame colors

--- home/dunst.nix
+++ home/dunst.nix
@@ -1,4 +1,12 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit (lib) mkForce;
+
+  colors = config.lib.stylix.colors.withHashtag;
+in {
   services.dunst = {
     enable = true;
 
@@ -9,6 +17,9 @@
         corner_radius = 10;
         offset = "10x10";
       };
+
+      urgency_low.frame_color = mkForce colors.base0B;
+      urgency_normal.frame_color = mkForce colors.base0D;
     };
     iconTheme = {
       inherit (config.stylix.icons) package name;
