3a5a7768a5032e070529d4eff0bde4e26e590f96
diniamo <diniamo53@gmail.com>
2024-06-05 21:27:30 +0200

    home/fzf: full theme

--- home/fzf.nix
+++ home/fzf.nix
@@ -5,7 +5,20 @@
     enable = true;
     enableZshIntegration = false;
 
-    colors."bg+" = config.lib.stylix.colors.withHashtag.base00;
+    colors = with config.lib.stylix.colors.withHashtag; {
+      fg = base05;
+      "fg+" = base06;
+      bg = base00;
+      "bg+" = base00;
+      hl = base0B;
+      "hl+" = base0B;
+      info = base0F;
+      marker = base0E;
+      prompt = base0D;
+      spinner = base0C;
+      pointer = base08;
+      header = base09;
+    };
     defaultOptions = ["--border=rounded"];
   };
 }
