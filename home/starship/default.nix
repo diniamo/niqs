7ee3282772b55af3c0700fa05dc316e71ae76583
diniamo <diniamo53@gmail.com>
2024-06-10 19:04:35 +0200

    home/starship: enable and configure time module

--- home/starship/default.nix
+++ home/starship/default.nix
@@ -6,7 +6,14 @@
     package = flakePkgs.niqspkgs.starship-nix3-shell;
 
     settings = {
+      format = "$directory$time$all";
+
       nix_shell.unknown_msg = "shell";
+      time = {
+        disabled = false;
+        time_format = "%R";
+        style = "bold purple";
+      };
     };
   };
 }
