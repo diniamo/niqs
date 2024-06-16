8363e088c6dc23649d75f4a3276b0d51e3b7f37b
diniamo <diniamo53@gmail.com>
2024-06-16 22:25:10 +0200

    home/lazygit: add keybind to delete both local and remote branch

--- home/lazygit.nix
+++ home/lazygit.nix
@@ -12,6 +12,22 @@
       };
       update.method = "never";
       promptToReturnFromSubprocess = false;
+
+      customCommands = [
+        {
+          key = "D";
+          context = "localBranches";
+          stream = true;
+          prompts = [
+            {
+              type = "confirm";
+              title = "Delete branch locally and remotely";
+              body = "Are you are you want to delete this branch both locally and remotely?";
+            }
+          ];
+          command = "git push {{ .SelectedLocalBranch.UpstreamRemote }} --delete {{ .SelectedLocalBranch.UpstreamBranch }} && git branch --delete {{ .SelectedLocalBranch.Name }}";
+        }
+      ];
     };
   };
 }
