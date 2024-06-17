29ebd4287ae76aaf63e057c0a13421d571c5dfbc
diniamo <diniamo53@gmail.com>
2024-06-17 19:12:28 +0200

    hosts/thinkpad: add networkmanager

--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -4,6 +4,7 @@
   imports = [ ./hardware.nix ];
 
   networking.hostName = "${config.values.mainUser}-THINKPAD";
+  networking.networkmanager.enable = true;
 
   system.stateVersion = "24.05"; # Did you read the comment?
 }
