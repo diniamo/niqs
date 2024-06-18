9806ed60ed720f89d857ec4156b2a7cf2f382070
diniamo <diniamo53@gmail.com>
2024-06-19 00:03:27 +0200

    modules/workstation(hyprland): remove TODO comment

--- modules/workstation/hyprland.nix
+++ modules/workstation/hyprland.nix
@@ -6,6 +6,5 @@
   imports = [inputs.hyprland.nixosModules.default];
 
   programs.hyprland.enable = true;
-  # TODO: does the module system have a built-in way to prepend to a list?
   xdg.portal.config.common.default = lib.mkForce ["hyprland" "kde"];
 }
