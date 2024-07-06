437ad32e23bbf300219fa3c47a1877db8bbcef6e
diniamo <diniamo53@gmail.com>
2024-07-06 20:30:22 +0200

    modules/workstation(system): enable the graphics module

--- modules/workstation/system.nix
+++ modules/workstation/system.nix
@@ -5,4 +5,8 @@
   };
 
   fonts.fontconfig.subpixel.rgba = "rgb";
+
+  # This is already enabled by modules that require it
+  # but I felt like enabling it anyway
+  hardware.graphics.enable = true;
 }
