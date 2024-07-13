027d07ceb0b120b70a50d131dc901b8f10904f40
diniamo <diniamo53@gmail.com>
2024-07-13 19:22:50 +0200

    home/nvf: enable treesitter folds

--- home/nvf/plugins/treesitter.nix
+++ home/nvf/plugins/treesitter.nix
@@ -1,5 +1,7 @@
 {pkgs, ...}: {
   programs.nvf.settings.vim.treesitter = {
+    fold = true;
+
     # Why is c a default?
     addDefaultGrammars = false;
     grammars = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
