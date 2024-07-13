4c41d5ed094c040957e97ab11e201fb0951a3ed6
diniamo <diniamo53@gmail.com>
2024-07-14 01:20:33 +0200

    home/fish: fix autopair plugin

--- home/fish/binds.nix
+++ home/fish/binds.nix
@@ -6,6 +6,7 @@
   programs.fish.functions.fish_user_key_bindings = lib.throwIf (lib.versionOlder "3.8" pkgs.fish.version) "Fish 3.8 has been released, change to using `accept-autosuggestion and execute`, then remove this error" ''
     fish_default_key_bindings -M insert
     fish_vi_key_bindings --no-erase
+    set -g fish_key_bindings fish_vi_key_bindings
 
     bind -M insert \cu kill-whole-line
 
