058f34bd48a0b3f24ef31ac79cabd88d050eb2c6
diniamo <diniamo53@gmail.com>
2024-07-14 20:04:55 +0200

    modules/shared(packages): use dash for binsh
    Supposedly NixOS relies on that to be bash, but most scripts are
    shebanged properly anyway, and I haven't see anything break.

--- modules/shared/packages.nix
+++ modules/shared/packages.nix
@@ -1,17 +1,25 @@
-{pkgs, lib, ...}: {
-  environment.defaultPackages = lib.mkForce [];
-  environment.systemPackages = with pkgs; [
-    htop
-    man-pages
-    man-pages-posix
-    ripgrep
-    fd
-    cht-sh
-    duf
-    jaq
-    wget
-    nh
-    nix-tree
-    nix-inspect
-  ];
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  environment = {
+    defaultPackages = lib.mkForce [];
+    binsh = lib.getExe pkgs.dash;
+
+    systemPackages = with pkgs; [
+      htop
+      man-pages
+      man-pages-posix
+      ripgrep
+      fd
+      cht-sh
+      duf
+      jaq
+      wget
+      nh
+      nix-tree
+      nix-inspect
+    ];
+  };
 }
