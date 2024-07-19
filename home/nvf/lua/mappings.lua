07cbe8f0cad239844443c6220a33e0c8304a5b8b
diniamo <diniamo53@gmail.com>
2024-07-19 18:38:18 +0200

    home/nvf: improve mappings.lua

--- home/nvf/lua/mappings.lua
+++ home/nvf/lua/mappings.lua
@@ -1,35 +1,41 @@
 -- TODO: move these to nix once a better way to deal with modes is implemented in nvf
 
+local map = function(mode, lhs, rhs, opts)
+    opts.noremap = true
+    opts.silent = true
+    vim.keymap.set(mode, lhs, rhs, opts)
+end
+
 -- Use screen lines for movement if we are only moving one line
-vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
-vim.keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true })
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true })
 
 -- Move lines
-vim.keymap.set("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move line down" })
-vim.keymap.set("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move line up" })
-vim.keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move line down" })
-vim.keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move line up" })
-vim.keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move lines down" })
-vim.keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move lines up" })
+map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move line down" })
+map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move line up" })
+map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move line down" })
+map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move line up" })
+map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move lines down" })
+map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move lines up" })
 
 -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
-vim.keymap.set("n", "n", "'Nn'[v:searchforward].'zv'", { expr = true, desc = "Next search result" })
-vim.keymap.set("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
-vim.keymap.set("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
-vim.keymap.set("n", "N", "'nN'[v:searchforward].'zv'", { expr = true, desc = "Prev search result" })
-vim.keymap.set("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
-vim.keymap.set("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
+map("n", "n", "'Nn'[v:searchforward].'zv'", { desc = "Next search result", expr = true })
+map("x", "n", "'Nn'[v:searchforward]", { desc = "Next search result", expr = true })
+map("o", "n", "'Nn'[v:searchforward]", { desc = "Next search result", expr = true })
+map("n", "N", "'nN'[v:searchforward].'zv'", { desc = "Prev search result", expr = true })
+map("x", "N", "'nN'[v:searchforward]", { desc = "Prev search result", expr = true })
+map("o", "N", "'nN'[v:searchforward]", { desc = "Prev search result", expr = true })
 
 -- Save file
-vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>silent w<cr><esc>", { desc = "Save file" })
+map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>silent w<cr><esc>", { desc = "Save file" })
 
 -- Window movement
-vim.keymap.set({ "n", "t" }, "<C-h>", "<C-w>h", { desc = "Go to left window", noremap = true })
-vim.keymap.set({ "n", "t" }, "<C-j>", "<C-w>j", { desc = "Go to lower window", noremap = true })
-vim.keymap.set({ "n", "t" }, "<C-k>", "<C-w>k", { desc = "Go to upper window", noremap = true })
-vim.keymap.set({ "n", "t" }, "<C-l>", "<C-w>l", { desc = "Go to right window", noremap = true })
+map({ "n", "t" }, "<C-h>", "<C-w>h", { desc = "Go to left window" })
+map({ "n", "t" }, "<C-j>", "<C-w>j", { desc = "Go to lower window" })
+map({ "n", "t" }, "<C-k>", "<C-w>k", { desc = "Go to upper window" })
+map({ "n", "t" }, "<C-l>", "<C-w>l", { desc = "Go to right window" })
 
-vim.keymap.set({ "n", "t" }, "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
-vim.keymap.set({ "n", "t" }, "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-vim.keymap.set({ "n", "t" }, "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
-vim.keymap.set({ "n", "t" }, "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+map({ "n", "t" }, "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+map({ "n", "t" }, "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+map({ "n", "t" }, "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+map({ "n", "t" }, "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
