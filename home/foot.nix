fa02a26fa13553586204f0ac1031ab7e1e5a79a6
diniamo <diniamo53@gmail.com>
2024-08-07 00:18:32 +0200

    home/foot: adjust for fish

--- home/foot.nix
+++ home/foot.nix
@@ -3,8 +3,6 @@
     enable = true;
     settings = {
       main = {
-        term = "xterm-256color";
-
         selection-target = "clipboard";
 
         # dpi-aware = true;
@@ -16,18 +14,4 @@
       mouse.hide-when-typing = "yes";
     };
   };
-
-  # This lets foot keep track the CWD
-  programs.zsh.initExtra = ''
-    osc7-pwd() {
-      emulate -L zsh # also sets localoptions for us
-      setopt extendedglob
-      local LC_ALL=C
-      printf '\e]7;file://%s%s\e\' $HOST ''${PWD//(#m)([^@-Za-z&-;_~])/%''${(l:2::0:)$(([##16]#MATCH))}}
-    }
-    chpwd-osc7-pwd() {
-      (( ZSH_SUBSHELL )) || osc7-pwd
-    }
-    add-zsh-hook -Uz chpwd chpwd-osc7-pwd
-  '';
 }
