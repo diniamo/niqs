fcde0c8222ec9589330a2152045519d8e8c22e70
diniamo <diniamo53@gmail.com>
2024-08-06 20:06:20 +0200

    home/walker: more config

--- home/walker.nix
+++ home/walker.nix
@@ -10,6 +10,26 @@
     package = flakePkgs.walker.default;
     runAsService = true;
 
-    # config = { };
+    config = {
+      theme_base = ["catppuccin"];
+
+      activation_mode.disabled = true;
+      ignore_mouse = true;
+    };
+
+    theme = {
+      layout = {
+        ui.window.box = {
+          v_align = "center";
+          orientation = "vertical";
+        };
+      };
+
+      style = ''
+        child {
+          border-radius: 0;
+        }
+      '';
+    };
   };
 }
