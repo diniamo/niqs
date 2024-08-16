9e5d28ea7eeff5c76dedf2964d0ba8f109f79603
diniamo <diniamo53@gmail.com>
2024-08-16 20:45:15 +0200

    home/sway: init
    Unused, as explicit sync doesn't work even on Sway, so I have no real
    reason to bother switching yet. God bless Nvidia.

--- home/default.nix
+++ home/default.nix
@@ -51,6 +51,7 @@ in {
     ./satty.nix
     # ./walker.nix
     ./fuzzel.nix
+    # ./sway.nix
   ];
 
   home = {
---
+++ home/sway.nix
@@ -0,0 +1,10 @@
+{
+  wayland.windowManager.sway = {
+    enable = true;
+    checkConfig = false;
+    # Uncomment and use the NixOS module if I actually switch
+    # package = null;
+
+    # config = { };
+  };
+}
