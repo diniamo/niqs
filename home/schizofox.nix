b55e51525aebf89897023b25d6814ddbae7bf37d
diniamo <diniamo53@gmail.com>
2024-09-01 12:07:43 +0200

    home/schizofox: remove vimium and youtube ad skip mute
    I'm gonna try tridactyl the next time I want vim binds in firefox

--- home/schizofox.nix
+++ home/schizofox.nix
@@ -81,13 +81,12 @@ in {
         "{c84d89d9-a826-4015-957b-affebd9eb603}".install_url = mkUrl "mal-sync";
         "{036a55b4-5e72-4d05-a06c-cba2dfcc134a}".install_url = mkUrl "traduzir-paginas-web";
         "idcac-pub@guus.ninja".install_url = mkUrl "istilldontcareaboutcookies";
-        "{d7742d87-e61d-4b78-b8a1-b469842139fa}".install_url = mkUrl "vimium-ff";
         "{96ef5869-e3ba-4d21-b86e-21b163096400}".install_url = mkUrl "font-fingerprint-defender";
         "{e90f5de4-8510-4515-9f67-3b6654e1e8c2}".install_url = mkUrl "dictionary-anywhere";
         "@hideyoutubecontrolls".install_url = mkUrl "hide-youtube-controls";
         "{762f9885-5a13-4abd-9c77-433dcd38b8fd}".install_url = mkUrl "return-youtube-dislikes";
         # "addons@wakatime.com".install_url = mkUrl "wakatimes";
-        "{6712269b-6289-46fe-b6f3-8144f45afd5e}".install_url = mkUrl "youtube-ad-skip-mute";
+        # "{6712269b-6289-46fe-b6f3-8144f45afd/* 5e */}".install_url = mkUrl "youtube-ad-skip-mute";
 
         # Disable
         "{c607c8df-14a7-4f28-894f-29e8722976af}" = null; # Temporary containers
