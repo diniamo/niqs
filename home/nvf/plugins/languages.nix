2c4dae3e215a2e638749946a3794bf82de8422a3
diniamo <diniamo53@gmail.com>
2024-09-13 19:33:24 +0200

    home/nvf: basedpyright settings

--- home/nvf/plugins/languages.nix
+++ home/nvf/plugins/languages.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   programs.nvf.settings.vim.languages = {
     enableExtraDiagnostics = true;
     enableFormat = true;
@@ -9,12 +9,28 @@
     lua.enable = true;
     # markdown.enable = true;
     nix.enable = true;
-    python.enable = true;
+    python = {
+      enable = true;
+      # nvf doesn't allow changing lsp settings currently
+      # so I'm forced to define the entire lsp myself, see below
+      lsp.enable = false;
+    };
     rust = {
       enable = true;
       crates.enable = true;
     };
-    clang.enable = true;
+    # clang.enable = true;
     markdown.enable = true;
+  };
+
+  programs.nvf.modules.lspSources = {
+    basedpyright = {
+      package = pkgs.basedpyright // {meta.mainProgram = "basedpyright-langserver";};
+      arguments = ["--stdio"];
+
+      settings = {
+        typeCheckingMode = "standard";
+      };
+    };
   };
 }
