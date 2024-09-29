20ef050b32b3b5b2211029a47ecd3ff3095a714e
diniamo <diniamo53@gmail.com>
2024-09-29 20:02:00 +0200

    home/fish: update binds to use new way of specifying keys

--- home/fish/binds.nix
+++ home/fish/binds.nix
@@ -4,21 +4,22 @@
     fish_vi_key_bindings --no-erase
     set -g fish_key_bindings fish_vi_key_bindings
 
-    bind -M insert \cu kill-whole-line
+    bind -M insert ctrl-u kill-whole-line
 
-    bind -M insert \b backward-kill-word
-    bind -M insert \e\[3\;5~ kill-word
+    bind -M insert ctrl-backspace backward-kill-word
+    bind -M insert ctrl-delete kill-word
 
-    bind -M insert -k nul accept-autosuggestion execute
+    bind -M default ctrl-space accept-autosuggestion execute
+    bind -M insert ctrl-space accept-autosuggestion execute
 
-    bind -M insert \e\[B history-search-forward
-    bind -M insert \e\[A history-search-backward
-    bind -M insert \n history-prefix-search-forward
-    bind -M insert \ck history-prefix-search-backward
-    bind -M insert \ej history-token-search-forward
-    bind -M insert \ek history-token-search-backward
+    bind -M insert down history-search-forward
+    bind -M insert up history-search-backward
+    bind -M insert ctrl-j history-prefix-search-forward
+    bind -M insert ctrl-k history-prefix-search-backward
+    bind -M insert alt-j history-token-search-forward
+    bind -M insert alt-k history-token-search-backward
 
-    bind -M insert \t 'if commandline -P; commandline -f complete; else; commandline -f complete-and-search; end'
-    bind -M insert \e\[Z complete-and-search
+    bind -M insert tab 'if commandline -P; commandline -f complete; else; commandline -f complete-and-search; end'
+    bind -M insert shift-tab complete-and-search
   '';
 }
