882d97ec0c608a842f1c337745227eca967b8eca
diniamo <diniamo53@gmail.com>
2024-10-05 18:02:04 +0200

    home/nvf: allow closing dap floats with q

--- home/nvf/lua/autocmds.lua
+++ home/nvf/lua/autocmds.lua
@@ -30,6 +30,7 @@ vim.api.nvim_create_autocmd("FileType", {
     "neotest-summary",
     "neotest-output-panel",
     "dbout",
+    "dap-float"
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
