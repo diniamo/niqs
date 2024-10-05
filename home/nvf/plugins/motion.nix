53458b81a7663abf731d509127be8afa77940b41
diniamo <diniamo53@gmail.com>
2024-10-05 18:02:33 +0200

    home/nvf: adjust flash settings

--- home/nvf/plugins/motion.nix
+++ home/nvf/plugins/motion.nix
@@ -1,9 +1,24 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  inherit (lib.generators) mkLuaInline;
+in {
   programs.nvf.modules.setupPlugins.flash = {
     package = pkgs.vimPlugins.flash-nvim;
     setupOpts = {
       prompt.enabled = false;
-      modes.char.autohide = true;
+      modes.char.char_actions = mkLuaInline ''
+        function(motion)
+          return {
+            [";"] = "right",
+            [","] = "left",
+            [motion] = "next",
+            [motion:match("%l") and motion:upper() or motion:lower()] = "prev",
+          }
+        end
+      '';
     };
   };
 }
