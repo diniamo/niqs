0cd68e6ea6e950470124eed83228b09860c9a5c4
diniamo <diniamo53@gmail.com>
2024-10-06 13:41:52 +0200

    home/nvf: disable flash in operator pending mode enitrely

--- home/nvf/plugins/motion.nix
+++ home/nvf/plugins/motion.nix
@@ -9,16 +9,24 @@ in {
     package = pkgs.vimPlugins.flash-nvim;
     setupOpts = {
       prompt.enabled = false;
-      modes.char.char_actions = mkLuaInline ''
-        function(motion)
-          return {
-            [";"] = "right",
-            [","] = "left",
-            [motion] = "next",
-            [motion:match("%l") and motion:upper() or motion:lower()] = "prev",
-          }
-        end
-      '';
+      modes.char = {
+        config = mkLuaInline ''
+          function(opts)
+            opts.autohide = opts.autohide or (vim.fn.mode(true) == "no")
+          end
+        '';
+
+        char_actions = mkLuaInline ''
+          function(motion)
+            return {
+              [";"] = "right",
+              [","] = "left",
+              [motion] = "next",
+              [motion:match("%l") and motion:upper() or motion:lower()] = "prev",
+            }
+          end
+        '';
+      };
     };
   };
 }
