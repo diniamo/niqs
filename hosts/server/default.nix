4327987946e2c7a9f676d132149b16c29d74e6f8
diniamo <diniamo53@gmail.com>
2024-10-19 21:04:08 +0200

    hosts/server: transmission, jellyfin

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -1,11 +1,38 @@
-{
+{pkgs,...}:{
   imports = [./hardware.nix];
 
   networking.hostName = "diniamo-SERVER";
 
   services = {
     satisfactory.enable = true;
+
+    transmission = {
+      enable = true;
+
+      openRPCPort = true;
+      webHome = pkgs.flood-for-transmission;
+      credentialsFile = "/var/lib/transmission/settings.json";
+      downloadDirPermissions = "755";
+
+      settings = {
+        rpc-bind-address = "0.0.0.0";
+        rpc-whitelist-enabled = false;
+        anti-brute-force-enabled = true;
+        rpc-authentication-required = true;
+
+        rename-partial-files = false;
+        incomplete-dir-enabled = false;
+        umask = 493; # 755 (octal) in decimal
+      };
+    };
+
+    jellyfin = {
+      enable = true;
+      openFirewall = true;
+    };
   };
 
-  system.stateVersion = "23.11"; # Did you read the comment?
+  systemd.services.transmission.serviceConfig.StateDirectoryMode = 755;
+
+  system.stateVersion = "23.11";
 }
