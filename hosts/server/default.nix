d843f32bc09059b636e2470538f6718f20a98120
diniamo <diniamo53@gmail.com>
2024-11-07 18:54:00 +0100

    hosts/server: static ip

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -1,7 +1,21 @@
 {
   imports = [./hardware.nix];
 
-  networking.hostName = "diniamo-SERVER";
+  networking = {
+    hostName = "diniamo-SERVER";
+
+    dhcpcd.enable = false;
+    interfaces.enp10s0.ipv4.addresses = [
+      {
+        address = "192.168.0.120";
+        prefixLength = 24;
+      }
+    ];
+    defaultGateway = {
+      interface = "enp10s0";
+      address = "192.168.0.1";
+    };
+  };
 
   services.satisfactory.enable = true;
 
