1a073be38d2cd85e77cf3bb25c44b3f15c110a6e
diniamo <diniamo53@gmail.com>
2024-12-03 21:04:18 +0100

    home/nvf: use lldb for zig debugging

--- home/nvf/plugins/languages.nix
+++ home/nvf/plugins/languages.nix
@@ -1,42 +1,49 @@
 {pkgs, ...}: {
-  programs.nvf.settings.vim.languages = {
-    enableExtraDiagnostics = true;
-    enableFormat = true;
-    enableLSP = true;
-    enableTreesitter = true;
+  programs.nvf = {
+    settings.vim = {
+      languages = {
+        enableExtraDiagnostics = true;
+        enableFormat = true;
+        enableLSP = true;
+        enableTreesitter = true;
 
-    bash.enable = true;
-    lua.enable = true;
-    nix.enable = true;
-    python = {
-      enable = true;
-      # nvf doesn't allow changing lsp settings currently,
-      # so I'm forced to define the entire lsp myself, see below
-      lsp.enable = false;
+        bash.enable = true;
+        lua.enable = true;
+        nix.enable = true;
+        python = {
+          enable = true;
+          # nvf doesn't allow changing lsp settings currently,
+          # so I'm forced to define the entire lsp myself, see below
+          lsp.enable = false;
+        };
+        rust = {
+          enable = true;
+          crates.enable = true;
+        };
+        clang.enable = true;
+        markdown.enable = true;
+        # nu.enable = true;
+        # csharp.enable = true;
+        # julia = {
+        #   enable = true;
+        #   # Provide it in devshells instead
+        #   lsp.package = null;
+        # };
+        zig.enable = true;
+      };
+
+      # HACK: figure out a good way to upstream this
+      debugger.nvim-dap.sources.zig-debugger = "dap.configurations.zig = dap.configurations.cpp";
     };
-    rust = {
-      enable = true;
-      crates.enable = true;
-    };
-    clang.enable = true;
-    markdown.enable = true;
-    # nu.enable = true;
-    # csharp.enable = true;
-    # julia = {
-    #   enable = true;
-    #   # Provide it in devshells instead
-    #   lsp.package = null;
-    # };
-    zig.enable = true;
-  };
 
-  programs.nvf.custom.lspSources = {
-    basedpyright = {
-      package = pkgs.basedpyright // {meta.mainProgram = "basedpyright-langserver";};
-      arguments = ["--stdio"];
+    custom.lspSources = {
+      basedpyright = {
+        package = pkgs.basedpyright // {meta.mainProgram = "basedpyright-langserver";};
+        arguments = ["--stdio"];
 
-      settings = {
-        typeCheckingMode = "standard";
+        settings = {
+          typeCheckingMode = "standard";
+        };
       };
     };
   };
