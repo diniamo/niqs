42de7c7ceaf035871151ee7c848f3eca8f7dbd17
diniamo <diniamo53@gmail.com>
2024-12-19 18:28:42 +0100

    home/nvf: remove usnused autocmd

--- home/nvf/lua/autocmds.lua
+++ home/nvf/lua/autocmds.lua
@@ -63,17 +63,6 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
--- Auto create dir when saving a file, in case some intermediate directory does not exist
-vim.api.nvim_create_autocmd("BufWritePre", {
-  callback = function(event)
-    if event.match:match("^%w%w+:[\\/][\\/]") then
-      return
-    end
-    local file = vim.uv.fs_realpath(event.match) or event.match
-    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
-  end,
-})
-
 -- Change CWD to opened directory
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function(data)
