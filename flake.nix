d9ca0a85675ffec3d06c124b4fd1f9c69c61f6d3
diniamo <diniamo53@gmail.com>
2024-12-26 08:59:06 +0100

    flake: update nvf

--- flake.nix
+++ flake.nix
@@ -166,8 +166,8 @@
     };
 
     nvf = {
-      # url = "github:NotAShelf/nvf";
-      url = "github:diniamo/nvf/custom";
+      url = "github:NotAShelf/nvf";
+      # url = "github:diniamo/nvf/custom";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         systems.follows = "systems";
