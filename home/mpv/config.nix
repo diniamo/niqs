64f73d276611ac1297a44c7be780ea3b3c337d99
diniamo <diniamo53@gmail.com>
2025-02-23 19:05:49 +0100

    home/mpv: set volume-max to 100
    Going above results in corruption (I think?), and I can usually just
    adjust the volume outside of mpv instead (but there is usually no need).

--- home/mpv/config.nix
+++ home/mpv/config.nix
@@ -9,6 +9,7 @@
       osd-duration = 3000;
       osd-status-msg = "Frame: \${estimated-frame-number} / \${estimated-frame-count}";
       audio-device = "pipewire";
+      volume-max = 100;
 
       ytdl-format = "bestvideo[height<=?1080]+bestaudio/best";
       ytdl-raw-options = "format-sort=\"proto:m3u8\",mark-watched=,cookies-from-browser=\"firefox\",user-agent=\"Mozilla/5.0\"";
