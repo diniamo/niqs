31b984d987f9de13fcb98db17bd0f1a3096001f5
diniamo <diniamo53@gmail.com>
2025-05-05 15:13:21 +0200

    home/gammastep: adjust times
    The grind is on

--- home/gammastep.nix
+++ home/gammastep.nix
@@ -2,8 +2,8 @@
   services.gammastep = {
     enable = true;
 
-    dawnTime = "5:00-6:30";
-    duskTime = "19:00-21:00";
+    dawnTime = "4:30-5:00";
+    duskTime = "19:00-20:00";
     temperature = {
       day = 6500;
       night = 2600;
