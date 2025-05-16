bca6b8ff48b5f1169952b8889a8a6a8759253701
diniamo <diniamo53@gmail.com>
2025-05-16 10:19:30 +0200

    home/jujutsu: tug, push, default-command, push-new-bookmarks

--- home/jujutsu.nix
+++ home/jujutsu.nix
@@ -2,12 +2,22 @@
   programs.jujutsu = {
     enable = true;
     settings = {
+      aliases = {
+        tug = ["bookmark" "move" "--from" "closest_bookmark(@-)" "--to" "@-"];
+        push = ["git" "push"];
+      };
+
+      revset-aliases = {
+        "closest_bookmark(to)" = "heads(::to & bookmarks())";
+      };
+
       user = {
         name = "diniamo";
         email = "diniamo53@gmail.com";
       };
 
       ui = {
+        default-command = "status";
         diff.tool = ["difft" "--color=always" "$left" "$right"];
         merge-editor = ["emacs" "--eval" "(ediff-merge-files-with-ancestor \"$left\" \"$right\" \"$base\" nil \"$output\")"];
       };
@@ -15,6 +25,7 @@
       git = {
         fetch = ["upstream" "origin"];
         push = "origin";
+        push-new-bookmarks = true;
       };
     };
   };
