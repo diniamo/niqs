5901e7a396320ee6f9aceef1a13ce2ecfe57276b
diniamo <diniamo53@gmail.com>
2025-05-24 21:25:57 +0200

    home/fish: nix-hash -> fix-hash, attempt to replace empty hash instead of just copying

--- home/fish/functions.nix
+++ home/fish/functions.nix
@@ -317,19 +317,49 @@
       '';
     };
 
-    nix-hash = {
-      description = "extract required hash from a failing Nix build command";
+    fix-hash = {
+      description = "extract required hash from a failing Nix command, and replace the empty hash with it";
       body = ''
         set -f output (script --quiet --command "$argv" /dev/null &| tee /dev/stderr | string split0)
+        echo
         if set -f hash (_extract_nix_hash $output)
-          echo "\nCopying $hash"
-          echo -n $hash | wl-copy
+          rg --type nix 'hash\s*=\s*""' --json | jaq --raw-output 'select(.type == "match") | "\(.data.path.text)	\(.data.line_number)"' | while read --delimiter '	' -l path line
+            set -fa paths $path
+            set -fa lines $line
+          end
+          
+          set -f count (count $paths)
+          if [ $count -gt 0 ]
+            if [ $count -gt 1 ]
+              for i in (seq $count)
+                echo -n [
+                set_color --bold; echo -n $i; set_color normal
+                echo "] $paths[$i]"
+              end
+              read -l answer --prompt-str 'Multiple empty hashes found. Which one would you like to replace? '
+              commandline --function repaint # Read messes up my actual prompt for some reason
+              
+              if not set -q paths[$answer] 2>/dev/null
+                set_color --bold red; echo "Invalid choice: $answer"
+                return 1
+              end
+              set -f paths $paths[$answer]
+              set -f lines $lines[$answer]
+            end
+
+            echo -n 'Replacing empty hash in '; set_color --bold; echo $paths; set_color normal
+            sed -i $lines"s|\(hash[[:space:]]*=[[:space:]]*\"\)|\1$hash|" $paths
+          else
+            set_color yellow; echo 'Failed to find location, falling back to copy'; set_color normal
+            echo "Copying $hash"
+            echo -n $hash | wl-copy
+          end
         else
           set_color --bold red; echo "No hash in output"; set_color normal
           return 1
         end
       '';
-      completion = "complete --command nix-hash --no-files --arguments '(_complete_command_offset)'";
+      completion = "complete --command fix-hash --no-files --arguments '(_complete_command_offset)'";
     };
     
     notify = {
