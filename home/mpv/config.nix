02ea47cf73bba7d43acdcb0ba50edb5ff1e89b45
diniamo <diniamo53@gmail.com>
2025-06-16 22:30:33 +0200

    home/mpv: always enable cache

--- home/mpv/config.nix
+++ home/mpv/config.nix
@@ -3,17 +3,22 @@
 in {
   programs.mpv = {
     config = {
+      cache = true;
+
       fullscreen = true;
-      no-keepaspect-window = "";
+      keepaspect-window = false;
+      window-dragging = false;
+      
+      audio-device = "pipewire";
+      volume-max = 100;
+      
       save-position-on-quit = "";
       watch-later-options-remove = "vf,af";
-      no-window-dragging = "";
+      
       osc = false;
       osd-font = font;
       osd-duration = 3000;
       osd-status-msg = "Frame: \${estimated-frame-number} / \${estimated-frame-count}";
-      audio-device = "pipewire";
-      volume-max = 100;
 
       ytdl-format = "bestvideo[height<=?1080]+bestaudio/best";
       ytdl-raw-options = "format-sort=\"proto:m3u8\",mark-watched=,cookies-from-browser=\"firefox\",user-agent=\"Mozilla/5.0\"";
