2593adc86c29fb5b1f649c216bfd65f1b3d1455a
diniamo <diniamo53@gmail.com>
2025-07-07 14:21:28 +0200

    workstation/librewolf: remove mal-sync, add unhook-youtube

--- modules/workstation/librewolf/extensions.nix
+++ modules/workstation/librewolf/extensions.nix
@@ -1,10 +1,10 @@
 {
   "{c2c003ee-bd69-42a2-b0e9-6f34222cb046}" = "auto-tab-discard";
-  "{c84d89d9-a826-4015-957b-affebd9eb603}" = "mal-sync";
   "{036a55b4-5e72-4d05-a06c-cba2dfcc134a}" = "traduzir-paginas-web";
   # "{e90f5de4-8510-4515-9f67-3b6654e1e8c2}" = "dictionary-anywhere";
   "{762f9885-5a13-4abd-9c77-433dcd38b8fd}" = "return-youtube-dislikes";
   "sponsorBlocker@ajay.app" = "sponsorblock";
   "@hideyoutubecontrolls" = "hide-youtube-controls";
   # "firefox@tampermonkey.net" = "tampermonkey";
+  "myallychou@gmail.com" = "youtube-recommended-videos";
 }
