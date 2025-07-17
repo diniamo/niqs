f2773a0734bf4ad19ea6b161bacbdf2517f4bc19
diniamo <diniamo53@gmail.com>
2025-07-17 06:48:40 +0200

    workstation/spoitfy-player: wrap instead of linking

--- modules/workstation/spotify-player/default.nix
+++ modules/workstation/spotify-player/default.nix
@@ -1,6 +1,8 @@
 { lib, pkgs, config, ... }: let
-  inherit (lib) mkEnableOption mkPackageOption mkOption mkIf;
-  inherit (lib.types) attrsOf listOf;
+  inherit (lib) mkEnableOption mkPackageOption mkOption mkIf optional;
+  inherit (lib.types) attrsOf listOf package;
+  inherit (pkgs) linkFarm symlinkJoin makeBinaryWrapper;
+  inherit (builtins) length;
 
   tomlFormat = pkgs.formats.toml {};
   tomlType = tomlFormat.type;
@@ -10,13 +12,37 @@
   appFile = tomlFormat.generate "spotify-player-app.toml" cfg.settings;
   themeFile = tomlFormat.generate "spotify-player-theme.toml" { inherit (cfg) themes; };
   keymapFile = tomlFormat.generate "spotify-player-keymap.toml" { inherit (cfg) keymaps actions; };
+
+  configLinks = optional (cfg.settings != {}) { name = "app.toml"; path = appFile; }
+    ++ optional (cfg.themes != []) { name = "theme.toml"; path = themeFile; }
+    ++ optional (cfg.keymaps != [] || cfg.actions != []) { name = "keymap.toml"; path = keymapFile; };
+  configDirectory = linkFarm "spotify-player-config" configLinks;
+
+  wrapped = symlinkJoin {
+    pname = "spotify-player-wrapped";
+    inherit (cfg.package) version meta;
+
+    paths = [ cfg.package ];
+
+    nativeBuildInputs = [ makeBinaryWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/spotify_player \
+        --add-flags '--config-folder ${configDirectory}'
+    '';
+  };
 in {
   imports = [ ./settings.nix ];
 
   options = {
     custom.spotify-player = {
       enable = mkEnableOption "spotify-player";
+      
       package = mkPackageOption pkgs "spotify-player" {};
+      finalPackage = mkOption {
+        type = package;
+        readOnly = true;
+        description = "The wrapped spotify-player package.";
+      };
 
       settings = mkOption {
         type = attrsOf tomlType;
@@ -44,12 +70,10 @@ in {
   };
 
   config = mkIf cfg.enable {
-    user.packages = [ cfg.package ];
+    custom.spotify-player.finalPackage =
+      if length configLinks > 0 then wrapped
+      else cfg.package;
 
-    home.files = {
-      ".config/spotify-player/app.toml" = mkIf (cfg.settings != {}) { source = appFile; };
-      ".config/spotify-player/theme.toml" = mkIf (cfg.themes != []) { source = themeFile; };
-      ".config/spotify-player/keymap.toml" = mkIf (cfg.keymaps != [] || cfg.actions != []) { source = keymapFile; };
-    };
+    user.packages = [ cfg.finalPackage ];
   };
 }
