e5d98905242171382c877b19932b99491f742cff
diniamo <diniamo53@gmail.com>
2025-07-23 12:05:39 +0200

    workstation/sway: use final swayimg package

--- modules/workstation/sway/settings.nix
+++ modules/workstation/sway/settings.nix
@@ -76,7 +76,7 @@ in {
       bindsym Control+Print exec ${grim} -g "$(${slurp})" - | ${wl-copy}
       bindsym Mod1+Print exec ${grim} -g "$(${swaymsg} -t get_tree | ${jq} -j '.. | select(.type?) | select(.focused).rect | "\(.x),\(.y) \(.width)x\(.height)"')"
       bindsym Print exec ${grim} -o "$(${swaymsg} -t get_outputs | ${jq} -r '.[] | select(.focused) | .name')" - | ${wl-copy}
-      bindsym Shift+Print exec ${grim} -g "$(${slurp})" - | ${getExe custom.swayimg.package} -
+      bindsym Shift+Print exec ${grim} -g "$(${slurp})" - | ${getExe custom.swayimg.finalPackage} -
       bindsym Mod4+o exec ${getExe' pkgs.wl-clipboard "wl-paste"} | ${getExe custom.satty.finalPackage} -f -
       bindsym Mod4+p exec ${getExe pkgs.hyprpicker} --autocopy
 
