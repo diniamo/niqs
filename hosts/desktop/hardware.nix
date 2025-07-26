60940a355e0e91aec3f700aae26bcb620b05b73c
diniamo <diniamo53@gmail.com>
2025-07-26 17:43:05 +0200

    hosts/desktop: update hardware config

--- hosts/desktop/hardware.nix
+++ hosts/desktop/hardware.nix
@@ -1,39 +1,27 @@
 # Don't change this
-{ config, lib, modulesPath, ... }: {
-  imports = [
-    (modulesPath + "/installer/scan/not-detected.nix")
-  ];
+{ config, lib, modulesPath, ... }: let
+  inherit (lib) mkDefault;
+in {
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
   boot = {
-    initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
-    initrd.kernelModules = [];
+    initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
     kernelModules = [ "kvm-intel" ];
-    extraModulePackages = [];
-    blacklistedKernelModules = [ "i2c_nvidia_gpu" ];
   };
 
   fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-uuid/57e169d1-9acc-42c3-b4ea-fd4080bc6a25";
-      fsType = "ext4";
-    };
     "/boot" = {
-      device = "/dev/disk/by-uuid/CE24-5E95";
+      device = "/dev/disk/by-uuid/4800-49A0";
       fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
     };
-    "/hdd" = {
-      device = "/dev/disk/by-label/hdd";
+    "/" = {
+      device = "/dev/disk/by-uuid/5a6043ab-f673-4390-98b9-0aadabf1b1b5";
       fsType = "ext4";
-    };
-    "/torrent" = {
-      device = "/dev/disk/by-label/torrent";
-      fsType = "ntfs-3g";
     };
   };
 
-  swapDevices = [];
-
-  networking.useDHCP = lib.mkDefault true;
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  networking.useDHCP = mkDefault true;
+  nixpkgs.hostPlatform = mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = mkDefault config.hardware.enableRedistributableFirmware;
 }
