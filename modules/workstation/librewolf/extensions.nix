4c19332b80f42e0bb555f6049bddd6f633eff8f0
diniamo <diniamo53@gmail.com>
2025-09-19 16:14:40 +0200

    workstation/librewolf: remove temporary containers

--- modules/workstation/librewolf/extensions.nix
+++ modules/workstation/librewolf/extensions.nix
@@ -3,5 +3,4 @@
   "{762f9885-5a13-4abd-9c77-433dcd38b8fd}" = "return-youtube-dislikes";
   "sponsorBlocker@ajay.app" = "sponsorblock";
   "@hideyoutubecontrolls" = "hide-youtube-controls";
-  "{c607c8df-14a7-4f28-894f-29e8722976af}" = "temporary-containers";
 }
