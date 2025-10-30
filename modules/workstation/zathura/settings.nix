903ff785dbb46f67bd44937e65eb75bc989b4d7d
diniamo <diniamo53@gmail.com>
2025-10-30 08:21:22 +0100

    workstation/zathura: more keybinds, hide statusbar by default

--- modules/workstation/zathura/settings.nix
+++ modules/workstation/zathura/settings.nix
@@ -10,13 +10,19 @@
       set = {
         vertical-center = true;
         selection-clipboard = "clipboard";
+        guioptions = "";
       };
 
       map = {
-        h = "feedkeys '<C-Left>'";
-        j = "feedkeys '<C-Down>'";
-        k = "feedkeys '<C-Up>'";
-        l = "feedkeys '<C-Right>'";
+        h = "feedkeys <C-Left>";
+        j = "feedkeys <C-Down>";
+        k = "feedkeys <C-Up>";
+        l = "feedkeys <C-Right>";
+        n = "navigate next";
+        p = "navigate previous";
+        i = "toggle_statusbar";
+        ge = "goto bottom";
+        "<Space>" = "snap_to_page";
       };
     };
   };
